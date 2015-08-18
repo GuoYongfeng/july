6cec0bd61dba9e4644e34afb476f1df45c61db1c
guoyongfeng <296512521@qq.com>
2015-08-18 13:12:54 +0800

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,51 @@
-var gulp = require('gulp'),
-   less = require('gulp-less');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync');
 
-var paths = {
-	'css': ['app/**/*.less', 'static/css'],
-	'js': 'app/**/*.js',
-	'html': 'app/**/*.html'
+var globs = {
+	'css': ['src/less/*.less', 'src/less/**/*.css'],
+	'js': 'src/**/*.js',
+	'html': 'src/**/*.html',
+	'image': 'src/images/**'
 };
 
-gulp.task('css', function () {
-   gulp.src(paths.css)
-      .pipe(less())
-      .pipe(gulp.dest('Public/Css'));
+gulp.task('less', function () {
+    gulp.src(globs.css)
+        .pipe(less())
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('js', function () {
-   gulp.src(paths.js)
-      .pipe(gulp.dest('Public/Js'));
+gulp.task('images', function(){
+    gulp.src(globs.image)
+        .pipe(gulp.dest('public/image'));
 });
 
-gulp.task('html', function () {
-   gulp.src(paths.html)
-      .pipe(gulp.dest('Public/View'));
+gulp.task('js', function(){
+    gulp.src(globs.js)
+        .pipe(gulp.dest('public'));
 });
 
-gulp.task('server', ['html', 'css', 'js']);
+gulp.task('view', function () {
+    gulp.src(globs.html)
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(globs.css, ['less']);
+    gulp.watch(globs.js, ['js']);
+    gulp.watch(globs.image, ['image']);
+    gulp.watch(globs.html, ['view']);
+});
+
+
+gulp.task('serve', function() {
+    browserSync({
+    	files: ['src/**/*.*', 'public/**/*.*'],
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['watch', 'serve']);
+
